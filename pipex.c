bcfcfe2eb7ca6c584ef6010097f1bf3f86a2c8a3
MarkosComK <markoscomks@gmail.com>
2024-07-16 17:00:09 +0100

    Several tests - understanding what the pdf wants exactly

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:00:21 by marsoare          #+#    #+#             */
-/*   Updated: 2024/07/16 16:20:02 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/07/16 16:29:45 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,9 @@
 
 int main(int agrc, char* argv[])
 {
-	int id1 = fork();
-	int id2 = fork();
-	if (id1 == 0 && id2 == 0){
-		return 0;
-		printf("last process\n");
-	}
-	printf("Process id: %d\n Parent id: %d \n fork1(%d) \n fork2(%d) \n\n", getpid(), getppid(), id1, id2);
-	while(wait(NULL) != -1 || errno != ECHILD)
-		printf("wait\n");
+	printf("printf1\n");
+	fork();
+	printf("printf2\n");
+	wait(NULL);
 	return 0;
 }
