d36edbcfbb1a49cee0e44713853739eff0e6514b
MarkosComK <markoscomks@gmail.com>
2024-07-16 16:24:56 +0100

    Done - several tests usung fork() to understand his behavior

--- pipex.c
+++ pipex.c
@@ -6,18 +6,25 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:00:21 by marsoare          #+#    #+#             */
-/*   Updated: 2024/07/16 13:46:00 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/07/16 16:20:02 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <errno.h>
 
-int main()
+int main(int agrc, char* argv[])
 {
-	int fk1 = fork();
-	if (fk1 != 0)
-		fork();
-	printf("this is the command %d\n", fk1);
+	int id1 = fork();
+	int id2 = fork();
+	if (id1 == 0 && id2 == 0){
+		return 0;
+		printf("last process\n");
+	}
+	printf("Process id: %d\n Parent id: %d \n fork1(%d) \n fork2(%d) \n\n", getpid(), getppid(), id1, id2);
+	while(wait(NULL) != -1 || errno != ECHILD)
+		printf("wait\n");
 	return 0;
 }
