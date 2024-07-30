f62a6aacd3cdd15df509cb64b20126a70cc90002
markoscomks <markoscomks@gmail.com>
2024-07-30 13:03:19 +0100

    Norm ok

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: marsoare <marsoare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:00:21 by marsoare          #+#    #+#             */
-/*   Updated: 2024/07/30 12:34:29 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/07/30 13:03:04 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	execute(char *cmd, char **env)
 {
 	char	**split_cmd;
 	char	*path;
-	
+
 	split_cmd = ft_split(cmd, ' ');
 	path = get_path(split_cmd[0], env);
 	if (execve(path, split_cmd, env) == -1)
@@ -31,17 +31,18 @@ void	execute(char *cmd, char **env)
 void	cmd1_execute(char **av, int *pipe_fds, char **env)
 {
 	int	fd;
+
 	fd = open(av[1], O_RDONLY, 0777);
 	dup2(fd, STDIN_FILENO);
 	dup2(pipe_fds[1], STDOUT_FILENO);
 	close(pipe_fds[0]);
 	execute(av[2], env);
-
 }
 
 void	cmd2_execute(char **av, int *pipe_fds, char **env)
 {
 	int	fd;
+
 	fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	dup2(pipe_fds[0], STDIN_FILENO);
 	dup2(fd, STDOUT_FILENO);
@@ -49,12 +50,12 @@ void	cmd2_execute(char **av, int *pipe_fds, char **env)
 	execute(av[3], env);
 }
 
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
-	(void) ac;
 	int		pipe_files[2];
 	pid_t	pid;
 
+	pid = fork();
 	if (ac != 5)
 	{
 		error_msg("Incorrect number of args");
@@ -62,7 +63,7 @@ int main(int ac, char **av, char **env)
 	}
 	if (pipe(pipe_files) == -1)
 		error_msg("error in pipe");
-	if ((pid = fork()) == -1)
+	if (pid == -1)
 		error_msg("error in fork");
 	if (!pid)
 		cmd1_execute(av, pipe_files, env);
