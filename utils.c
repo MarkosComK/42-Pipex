e7ef04d4ac77c475f958d4c249e4e4087a585c00
markoscomks <markoscomks@gmail.com>
2024-07-30 10:56:33 +0100

    Filtered env

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 19:29:46 by marsoare          #+#    #+#             */
-/*   Updated: 2024/07/25 21:51:12 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/07/30 10:56:02 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,32 @@ void	error_msg(char *str)
 	ft_putchar_fd('\n', 2);
 	ft_putstr_fd("\33[0m", 2);
 }
-char	*ft_getenv(char *name, char **env)
+char	*filterenv(char *name, char **env)
 {
 	int		i;
-	int		j;
 
 	i = 0;
-	while (env[i])
-	{
-		//walk over the env and return everyting after the PATH=
-	}
+	while (ft_strncmp(name, env[i], 4))
+		i++;
+	write(2, env[i], ft_strlen(env[i]));
 	return (NULL);
 }
 
 char	*get_path(char *cmd, char **env)
 {
-	//allpaths = ft_split(my_getenv("PATH", env), ':');
+	char	**paths;
+	int		i;
+
+	paths = ft_split(filterenv("PATH", env), ':');
+	i = 0;
 	//char *path = allpath[0];
 	/*
 	while(allpath++)
 		write(2, *allpath, ft_strlen(*allpath));
 	s_cmd = ft_split(cmd, ' ');
 	*/
-	while (allpath[i])
+	/*
+	while (paths[i])
 	{
 		//check somehow each path to find if the command are in it
 		if (access(exec, F_OK | X_OK) == 0)
@@ -64,7 +67,9 @@ char	*get_path(char *cmd, char **env)
 		free(exec);
 	}
 	// free everything if dont and handle the error
-	free_tab(allpath);
+	free_ta(allpath);
 	free_tab(s_cmd);
+	return (cmd);
+	*/
 	return (cmd);
 }
