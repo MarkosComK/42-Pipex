f012b15c6ba53593700aee233c08b9159b67dc32
markoscomks <markoscomks@gmail.com>
2024-08-15 16:39:19 +0100

    Norm ok

--- includes/bonus.h
+++ includes/bonus.h
@@ -10,9 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_BONUS_H
-# define PIPEX_BONUS_H
-
+#ifndef BONUS_H
+# define BONUS_H
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdbool.h>
@@ -29,7 +28,6 @@
 void	close_pipes(int **fd);
 void	free_pipes(int **fd);
 void	error_msg(char *msg, int **fd, char *str, int exit_status);
-
 void	free_tab(char **tab);
 char	*get_path(char *cmd, char **env);
 char	*my_getenv(char *name, char **env);
