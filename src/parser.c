04625c5b587847c46da958b0cf39253c3b9f8a16
markoscomks <markoscomks@gmail.com>
2024-08-24 21:05:04 +0100

    Started parser func

--- src/parser.c
+++ src/parser.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/24 20:48:40 by marsoare          #+#    #+#             */
-/*   Updated: 2024/08/24 21:02:09 by marsoare         ###   ########.fr       */
+/*   Created: 2024/08/24 21:03:22 by marsoare          #+#    #+#             */
+/*   Updated: 2024/08/24 21:04:46 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+char	**quote_space_parser(int argc, char **argv)
 {
-	int		exit_code;
+	int	i;
+	int	count;
 
-	argv = quote_space_parser(argc, argv);
-	return (exit_code);
+	i = 0;
+	if (argc > 1)
+	{
+		while (argv[i])
+		{
+			count = counter(argv[i], 39);
+			if (count & 1)
+			{
+				ft_putstr_fd("Quote error (odd number). Pipex exiting.\n", 2);
+				exit(EXIT_FAILURE);
+			}
+			argv[i] = space_to_minus(argv[i]);
+			argv[i] = dequote_single(argv[i]);
+			i++;
+		}
+	}
+	return (argv);
 }
