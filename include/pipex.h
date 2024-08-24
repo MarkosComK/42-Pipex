ed337dc58b128357b830b17c0b789f4ecc55cf3e
markoscomks <markoscomks@gmail.com>
2024-08-24 20:56:47 +0100

    Added colors for fomatation

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 20:12:30 by marsoare          #+#    #+#             */
-/*   Updated: 2024/08/24 20:16:14 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/08/24 20:56:33 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define PIPEX_H
 
 #include "../libft/libft.h"
+
+/* colors */
+# define RED "\033[31m"
+# define REDBOLD "\033[1;31m"
+# define GREEN "\033[32m"
+# define GREENBOLD "\033[1;32m"
+# define BOLD "\033[1m"
+# define UNDRLN "\033[4m"
+# define END "\033[0m"
+
 
 #endif
